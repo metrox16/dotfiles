da219814579f8b79a8c4d6d6b30672fbfdb6d8ed
Barnabás Vass <barnabas.vass@ericsson.com>
2026-09-17 15:02:24 +0200

    Added bat alias
    Change-Id: Id73bed6daa0269fdb5d2cfd42cb28a6bd421c9d0

--- bat/aliases.sh
+++ bat/aliases.sh
@@ -7,6 +7,8 @@ alias ccat='bat -pp'
 
 # Render --help output with syntax highlighting: cmd --help | bh
 alias bh='bat -l help'
+alias b='bat -pp -l'
+
 
 # Colourised man pages.
 export MANPAGER="sh -c 'col -bx | bat -l man -p --theme=gruvbox-dark'"

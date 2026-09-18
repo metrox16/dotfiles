f3ebea7255fc9524fbf1936a07f0b3da4982974a
Barnabás Vass <barnabas.vass@ericsson.com>
2026-09-18 14:30:12 +0200

    Added bat theme alias
    Change-Id: Ie11a2ed2d766a127e69f0053a089a85d481b38c1

--- bat/aliases.sh
+++ bat/aliases.sh
@@ -8,7 +8,7 @@ alias ccat='bat -pp'
 # Render --help output with syntax highlighting: cmd --help | bh
 alias bh='bat -l help'
 alias b='bat -pp -l'
-
+alias battheme='bat --list-themes | fzf --preview="bat --theme={} --color=always ~/.bashrc.sh"'
 
 # Colourised man pages.
 export MANPAGER="sh -c 'col -bx | bat -l man -p --theme=gruvbox-dark'"
